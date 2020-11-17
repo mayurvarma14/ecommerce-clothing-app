325b9d2b65858d0ce11cdc0ad500c8a488b9a5ea
Mayur Varma <mayurvarma14@gmail.com>
2020-11-17 21:10:17 +0530

    fix: typo in reducer

--- src/redux/cart/cartSelectors.js
+++ src/redux/cart/cartSelectors.js
@@ -23,8 +23,8 @@ export const selectCartItemsCount = createSelector(
 
 export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce(
-    (accumalatedQuantity, cartItem) =>
-      accumalatedQuantity + cartItem.quantity * cartItem.price,
+    (accumulatedQuantity, cartItem) =>
+      accumulatedQuantity + cartItem.quantity * cartItem.price,
     0
   )
 );
